35813b997c105d883985412328c78f80a3e3e2f0
ardasaracgm <ardasaracgm@gmail.com>
2026-05-02 01:08:54 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Menu, X, MessageCircle, MapPin, Phone, Mail, Instagram, ChevronLeft, ChevronRight } from "lucide-react";
+
 
 const heroSlides = [
   {
